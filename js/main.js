34eaf7536a07275877dad7852421969cb2386c89
HensyHugo <135879898+HensyHugo@users.noreply.github.com>
2025-02-10 18:03:57 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ function goTimerError(time) {
 }
 
 getSignal.onclick = function () {
-    let receivingSignal = getRandomFloat(1, 3.99, 2);
+    let receivingSignal = 1.72 ;
     if (receivingSignal.toString().length == 3) {
         receivingSignal += "0";
     }
